fa37d5f17497afeb3939edf16eae50d7bfe455b2
beniskova <hana.rottova@gmail.com>
2021-04-13 19:02:29 +0200

    Update main.js
    finish

--- main.js
+++ main.js
@@ -6,3 +6,27 @@
 // Z každého vylosovaného čísla vygeneruj následující HTML kód:
 // <span class="cislo">8</span>
 // ... který pak přidej dovnitř prvku <div id="vyherni-cisla">:
+
+let osudi = [];
+
+for (let i = 1; i <= 49; i = i + 1) {
+    osudi.push(i);
+}
+// z osudí vyberu 7 náhodných čísel
+
+let tazenaCisla = [];
+for (let i = 1; i <= 7; i++) {
+    let vyherniIndex = Math.floor(Math.random() * osudi.length);
+    let vyherniCislo = osudi[vyherniIndex];
+
+    tazenaCisla.push(vyherniCislo);
+    osudi.splice(vyherniIndex, 1);
+
+}
+
+//vypíšu do HTML
+let vyherniCisla = document.querySelector('#vyherni-cisla');
+
+for (let i = 0; i < tazenaCisla.length; i++) {
+    vyherniCisla.innerHTML += '<span class="cislo">' + tazenaCisla[i] + '</span >';
+}
